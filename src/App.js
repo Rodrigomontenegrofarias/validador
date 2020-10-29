2274059b147778b9364a0ec12680d57147ecbb9d
Rodrigo-Montenegro-Farias <59695256+Rodrigomontenegrofarias@users.noreply.github.com>
2020-10-29 13:06:10 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
 import logo from './logo.svg';
-import './App.css';
-
-import {Button} from "@material-ui/core"
-import MiComponente from './MiComponente';
-import MiValidador from './MiValidador';
+import "./App.css";
+//import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap';
+//import { Button } from "@material-ui/core";
+import MiComponente from "./MiComponents";
+import MiValidador from "./MiValidador";
 
 function App() {
-  return (
-    <div className="App">n
-      <header className="App-header">
-        <p>Clave uvalpo</p>
-        <MiValidador  valor = "123344" />
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+
+        <
+        header className = "App-header" >
+
+        <
+        p >
+
+
+        Contraseña....de uvalpo
+
+        <
+        /p > <
+        MiValidador valor = "193385745" / >
+        <
+        /header> < /
+        div >
+    );
 }
-
-
 
 export default App;
