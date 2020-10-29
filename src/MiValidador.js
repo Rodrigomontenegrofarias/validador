df40ede789890e9484041e677dbebf43c7db33e6
Rodrigo-Montenegro-Farias <59695256+Rodrigomontenegrofarias@users.noreply.github.com>
2020-10-29 13:16:09 -0300

    Update MiValidador.js

--- src/MiValidador.js
+++ src/MiValidador.js
@@ -1,27 +1,41 @@
+// eslint-disable-next-line no-unused-expressions
+'use strict'
 import React from 'react';
-import { validate } from "./rut"
+import { validate } from "./rut";
 export default function MiValidador(params) {
-    
     const [rutIngresado, setRutIngresado] = React.useState(params.valor);
+    console.log(rutIngresado);
+
 
     const [clave, setClave] = React.useState("");
+    console.log(clave);
 
-    
 
-    const change = (e)=>{
+
+
+    const change = (e) => {
         setRutIngresado(e.target.value)
     }
-    const onChangePassword = (e)=>{
-        setClave(e.target.value);
+
+    const onChangePassword = (e) => {
+        setClave(e.target.value)
     }
 
-     if(clave === "uvalpo") return <p>Maravilloso</p>
 
-     return  (<div>
-        <input type="text" value={rutIngresado} onChange={change}  />
-       { validate(rutIngresado) && 
-           <input type="password"  value={clave} onChange={onChangePassword}  />
-           
-       }
-       </div>)
+    if (clave == "uvalpo") return <p > Maravilloso < /p>
+    return ( < div >
+        <
+        input type = "text"
+        value = { rutIngresado }
+        onChange = { change }
+        / > { validate(rutIngresado) &&  <
+        input type = "password"
+        value = { clave }
+        // eslint-disable-next-line no-undef
+        onChange = { onChangePassword }
+
+        / >
+
+    } < /
+    div > )
 }
